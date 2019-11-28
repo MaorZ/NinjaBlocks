8659a682d69ea6d09ddc1c468af11e14f3ef8723
MaorZ <maorz1@gmail.com>
2019-11-29 01:42:25 +0200

    Ep2 code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
 import { TweenLite } from 'gsap';
-import {} from 'rxjs';
+import { fromEvent } from 'rxjs';
 import {} from 'rxjs/operators';
 
+let player = document.getElementById('player');
+
 console.log('Welcome to Ninja Blocks');
+
+fromEvent(document, 'keydown').subscribe((e: KeyboardEvent) => {
+  switch (e.key) {
+    case 'w':
+      TweenLite.to(player, {
+        ease: 'power0.none',
+        y: '-=5rem'
+      });
+      break;
+    case 's':
+      TweenLite.to(player, {
+        ease: 'power0.none',
+        y: '+=5rem'
+      });
+      break;
+    case 'a':
+      TweenLite.to(player, {
+        ease: 'power0.none',
+        x: '-=5rem'
+      });
+      break;
+    case 'd':
+      TweenLite.to(player, {
+        ease: 'power0.none',
+        x: '+=5rem'
+      });
+      break;
+  }
+});
