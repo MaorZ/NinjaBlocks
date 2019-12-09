2785b82905c457ba63a88f5abe3163947342abc6
MaorZ <maorz1@gmail.com>
2019-12-09 15:34:07 +0200

    Added Evade Effect

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ interface MotionVector {
 let player = document.getElementById('player');
 let levelContainer = document.getElementById('levelContainer');
 let playerIsMoving = false;
+let tick = 0;
 focusOnPlayer();
 
 console.log('Welcome to Ninja Blocks');
@@ -22,9 +23,19 @@ fromEvent(document, 'keydown').subscribe((e: KeyboardEvent) => {
         moveTo(levelContainer, { y: '+=5rem' });
         playerIsMoving = true;
         break;
+      case 'W':
+        moveTo(player, { y: '-=10rem' }, true);
+        moveTo(levelContainer, { y: '+=10rem' });
+        playerIsMoving = true;
+        break;
       case 's':
         moveTo(player, { y: '+=5rem' });
         moveTo(levelContainer, { y: '-=5rem' });
+        playerIsMoving = true;
+        break;
+      case 'S':
+        moveTo(player, { y: '+=10rem' }, true);
+        moveTo(levelContainer, { y: '-=10rem' });
         playerIsMoving = true;
         break;
       case 'a':
@@ -32,24 +43,60 @@ fromEvent(document, 'keydown').subscribe((e: KeyboardEvent) => {
         moveTo(levelContainer, { x: '+=5rem' });
         playerIsMoving = true;
         break;
+      case 'A':
+        moveTo(player, { x: '-=10rem' }, true);
+        moveTo(levelContainer, { x: '+=10rem' });
+        playerIsMoving = true;
+        break;
       case 'd':
         moveTo(player, { x: '+=5rem' });
         moveTo(levelContainer, { x: '-=5rem' });
+        playerIsMoving = true;
+        break;
+      case 'D':
+        moveTo(player, { x: '+=10rem' }, true);
+        moveTo(levelContainer, { x: '-=10rem' });
         playerIsMoving = true;
         break;
     }
   }
 });
 
-function moveTo(target: HTMLElement, motionVector: MotionVector) {
+function moveTo(
+  target: HTMLElement,
+  motionVector: MotionVector,
+  evadeEffect: boolean = false
+) {
   let moveTweenVars: GSAPStatic.tweenVars = {
-    ease: 'power0.none',
+    ease: 'none',
     duration: 0.3,
     ...motionVector,
     onComplete: () => {
       playerIsMoving = false;
     }
   };
+
+  if (evadeEffect) {
+    moveTweenVars.onUpdateParams = [target];
+    moveTweenVars.onUpdate = (target: HTMLElement) => {
+      tick++;
+      if (tick % 6 === 3) {
+        let clone = target.cloneNode() as HTMLElement;
+        target.parentElement.appendChild(clone);
+        clone.style.opacity = '0.8';
+        TweenLite.to(clone, {
+          duration: 0.5,
+          opacity: 0,
+          scaleX: 0.5,
+          onCompleteParams: [clone],
+          onComplete: (clone: HTMLElement) => {
+            clone.remove();
+            tick = 0;
+          }
+        });
+      }
+    };
+  }
 
   TweenLite.to(target, moveTweenVars);
 }
